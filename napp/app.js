c26e9c8f8d27c91c4b0510264d8617613b2fccc7
Alex Dumitrescu <ad1444@my.bristol.ac.uk>
2014-11-22 14:29:27 +0000

    news sent to frontend

--- napp/app.js
+++ napp/app.js
@@ -56,5 +56,9 @@ app.use(function(err, req, res, next) {
     });
 });
 
+ var port = process.env.PORT || 5000;
+ app.listen(port, function() {
+   console.log("Listening on " + port);
+ });
 
-module.exports = app;
+//module.exports = app;
--- napp/routes/index.js
+++ napp/routes/index.js
@@ -1,13 +1,59 @@
 var express = require('express');
+var MSF = require('../MSF');
 var router = express.Router();
+
+
+var username = "eikonstudent2@thomsonreuters.com";
+var password = "Secret123";
+var msf = new MSF(true);
+
+
+var topNews= {
+   "Entity": {
+       "E": "TopNews",
+       "W": {
+           "Codes": [
+               "urn:newsml:reuters.com:20020923:SPDOC_119827232002"
+           ],
+           "LoadImages": true
+       },
+       "S": [
+           "TopNews",
+           "TopNews.Headline",
+           "TopNews.StoryDateTime",
+           "TopNews.RelatedStories.CreationDateAndTime",
+           "TopNews.RelatedStories.HeadLine",
+           "TopNews.RelatedStories.TopicSet",
+           "TopNews.Brief",
+           "TopNews.StoryId"
+       ]
+   }
+}
 
 /* GET home page. */
 router.get('/', function(req, res) {
   res.render('index', { title: 'Express' });
 });
 
-router.get('/login', function(req, res) {
-  res.render('index', { title: 'Express' });
+router.get('/home', function(req, res) {
+    msf.login(username, password, function(error) {
+        if (!error) {
+            msf.getData(topNews, function(error, response) {
+                if (!error) {
+                    res.render('index.ejs', { news: JSON.stringify(response,false,2)});
+                } else {
+                    console.log("Error Getting Data: "+error);
+                     res.render('index.ejs', { news: "error"});
+                }
+            });
+        } else {
+            console.log("Error Login: "+error);
+            res.render('index.ejs', { news: "error"});
+        }
+    });
+   
+
+  
 });
 
 module.exports = router;
